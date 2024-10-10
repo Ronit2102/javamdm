a24f46c7b638124045ce820a95f12ebeb0e1b373
Ronit2102 <2023.ronit.sahoo@ves.ac.in>
2024-10-10 20:18:27 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins!");
+    }
+}
+2. Create a Git Repository:
+Once your Java file is created, you need to set up a Git repository and push the code to GitHub.
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin <your-git-repository-url>
+git push -u origin master
